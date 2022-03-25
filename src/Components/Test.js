ba3a862f7cc23aee792d06a37d43d3fe5fbb48c4
Aamir Bhat <aamirbhat382@gmail.com>
2022-03-25 17:54:01 +0000

    add new prayer times in test mode

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,136 +1,82 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import logo from "./logo.svg";
+import React ,{useState} from "react";
+import adhan from 'adhan'
+import moment  from "moment";
+
 
 function Test() {
-  const currentPathName = window.location.pathname.slice(1);
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [status, setStatus] = useState(null);
+  const showTimes = ()=>{
+    const date = new Date();
+    let coordinates = new adhan.Coordinates(latitude,longitude )
+    let params = adhan.CalculationMethod.Karachi()
+    params.madhab = adhan.Madhab.Hanafi;
+    const prayerTimes = new adhan.PrayerTimes(coordinates, date, params);
+    const fajrTime = moment(prayerTimes.fajr).format('h:mm A');
+    const sunriseTime = moment(prayerTimes.sunrise).format('h:mm A');
+    const dhuhrTime = moment(prayerTimes.dhuhr).format('h:mm A');
+    const asrTime = moment(prayerTimes.asr).format('h:mm A');
+    const maghribTime = moment(prayerTimes.maghrib).format('h:mm A');
+    const ishaTime = moment(prayerTimes.isha).format('h:mm A');
+    const current = prayerTimes.currentPrayer();
+    const next = prayerTimes.nextPrayer();
+    const nextPrayerTime = prayerTimes.timeForPrayer(next);
+    console.log(nextPrayerTime)
+    return (
+        <div className="container">
+          <h1>{next}</h1>
+         
+          <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Time</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Fajr</th>
+      <td>{fajrTime}</td>
+    </tr>
+    <tr>
+      <th scope="row">Sun rise</th>
+      <td>{sunriseTime}</td>
+    </tr>
+    <tr>
+      <th scope="row">Dhuhr</th>
+      <td>{dhuhrTime}</td>
+    </tr>
+    <tr>
+      <th scope="row">Asr</th>
+      <td>{asrTime}</td>
+    </tr>
+    <tr>
+      <th scope="row">Maghrib</th>
+      <td>{maghribTime}</td>
+    </tr>
+    <tr>
+      <th scope="row">Isha</th>
+      <td>{ishaTime}</td>
+    </tr>
+  </tbody>
+</table>
+        </div>
+    )
+  }
+  
+  if ('geolocation' in navigator) {
+    window.navigator.geolocation.getCurrentPosition(showPosition);
+    function showPosition(position) {
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude);
+      setStatus("locationAvaible")
+    }
+    }
+  
+  
   return (
-    <>
-      {/* NavBar Start */}
-      <nav className="navbar navbar-expand-md  ">
-        <div className="container-fluid">
-          <a
-            className="nav-link p-0"
-            data-bs-toggle="offcanvas"
-            href="/#offcanvasMenu"
-            role="button"
-            aria-controls="offcanvasMenu"
-          >
-            <i
-              className="bi bi-text-left rtl-flip fs-2"
-              data-bs-target="#offcanvasMenu"
-            >
-              {" "}
-            </i>
-          </a>
-          <Link className="navbar-brand text-capitalize" to="#">
-            {currentPathName}
-          </Link>
-        </div>
-      </nav>
-      {/* NavBar End */}
-      {/* Sidebar */}
-      <div
-        className="d-flex flex-column flex-shrink-0 p-3 bg-light sidebar offcanvas offcanvas-end-left"
-        id="offcanvasMenu"
-        style={{ visibility: "hidden" }}
-      >
-        <div className="offcanvas-header justify-content-end">
-          <button
-            type="button"
-            className="btn-close text-reset"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-          ></button>
-        </div>
-        <a
-          href="/"
-          className="d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
-        >
-          <img src={logo} className="App-logo" alt="logo" />
-        </a>
-        <hr />
-        <ul className="nav nav-pills flex-column mb-auto">
-          <li className="nav-item">
-            <NavLink to="/" className="nav-link " activeClassName="active" aria-current="page">
-            <i class="bi bi-house me-2"></i>
-              Home
-            </NavLink>
-          </li>
-          <li>
-          <NavLink to="/suraha/tajweed" className="nav-link " activeClassName="active" aria-current="page">
-          <i class="bi bi-book me-2"></i>
-              Tajweed
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/prayer" className="nav-link " activeClassName="active" aria-current="page">
-            <i class="bi bi-clock me-2"></i>
-              Prayer
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/clander" className="nav-link " activeClassName="active" aria-current="page">
-            <i class="bi bi-calendar-day me-2"></i>
-              Calendar
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink to="/about" className="nav-link " activeClassName="active" aria-current="page">
-            <i class="bi bi-file-earmark-person me-2"></i>
-              About
-            </NavLink>
-          </li>
-        </ul>
-        <hr />
-        <div className="dropdown">
-          <a
-            href="/"
-            className="d-flex align-items-center link-dark text-decoration-none dropdown-toggle"
-            id="dropdownUser2"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            <img
-              src="https://github.com/mdo.png"
-              alt=""
-              width="32"
-              height="32"
-              className="rounded-circle me-2"
-            />
-            <strong>Aamir Hussain</strong>
-          </a>
-          <ul
-            className="dropdown-menu text-small shadow"
-            aria-labelledby="dropdownUser2"
-          >
-            <li>
-              <a className="dropdown-item" href="/">
-                New project...
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="/">
-                Settings
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="/">
-                Profile
-              </a>
-            </li>
-            <li>
-              <hr className="dropdown-divider" />
-            </li>
-            <li>
-              <a className="dropdown-item" href="/">
-                Sign out
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </>
+   showTimes()
   );
 }
 
